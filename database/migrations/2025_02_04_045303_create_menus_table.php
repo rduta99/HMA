de82160e869f5331924dff198d10f712804eed12
Ryan snsv <arsuma24@gmail.com>
2025-02-04 15:41:13 +0700

    Fix Migration

--- database/migrations/2025_02_04_045303_create_menus_table.php
+++ database/migrations/2025_02_04_045303_create_menus_table.php
@@ -11,14 +11,14 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('menus', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('id_menu')->nullable();
             $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('icon');
+            $table->integer('order');
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
@@ -27,6 +27,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('menus');
     }
 };
